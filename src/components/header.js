a15fe3ad764cdffe7b6fe4a1903059b0d5ed3fca
Jimmy Lin <usmcjimmy@gmail.com>
2016-04-24 09:54:57 -0400

    initial setup for news web app

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+export default Header;
